556e145a2e493d02b3d9ba4d3fece846fc927d20
Merhi <Themerhi@hotmail.com>
2023-11-04 16:35:23 +0200

    1st choice added

--- FCS Assigments/03_Karim_Merhi.py
+++ FCS Assigments/03_Karim_Merhi.py
@@ -11,20 +11,29 @@ student_data = [
  {
 "ID": 4,"Name": "Bakri","Age": 23,"Major": "BITM","GPA": 1.6
  },
-  {
+{
 "ID": 5,"Name": "Lotfi","Age": 18,"Major": "HCIS","GPA": 2.7
  },
 ]
 def main():
-    print('''1. Get Student by ID
-2. Get All Students
-3. Get Students by Major
-4. Add Student
-5. Find Common Majors
-6. Delete Student
-7. Calculate Average GPA
-8. Get Top Performers
-9. Exit
-- - - - - - - ''')
-    
+    print('''
+ 1. Get Student by ID
+ 2. Get All Students
+ 3. Get Students by Major
+ 4. Add Student
+ 5. Find Common Majors
+ 6. Delete Student
+ 7. Calculate Average GPA
+ 8. Get Top Performers
+ 9. Exit
+ - - - - - - - ''')
+    choice=int(input("Enter Choice:"))
+    if (choice==1):
+        hashmapId=int(input("Enter ID:"))
+        for item in student_data:
+            if(item["ID"]== hashmapId):
+                print(item)
+    elif(choice==2):
+        print(student_data)
+
 main()
